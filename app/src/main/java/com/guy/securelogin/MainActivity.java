9eda6a6e86964012aaca8a8fac4d964d9d235005
Guy <guyskv@gmail.com>
2020-03-09 18:57:09 +0200

    Initial commit

---
+++ app/src/main/java/com/guy/securelogin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.guy.securelogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
